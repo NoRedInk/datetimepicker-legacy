15e0d6c94b4dd69004f78c2636c9b1d590d91df3
Luke Westby <lukewestby1@gmail.com>
2018-09-14 12:23:17 -0700

    delete redundant monthToInt

--- src/DateTimePicker/DateUtils.elm
+++ src/DateTimePicker/DateUtils.elm
@@ -6,7 +6,6 @@ module DateTimePicker.DateUtils
         , fromMillitaryAmPm
         , fromMillitaryHour
         , generateCalendar
-        , monthToInt
         , padding
         , toMillitary
         )
@@ -14,46 +13,6 @@ module DateTimePicker.DateUtils
 import Date
 import DateTimePicker.DateTime as DateTime
 import String
-
-
-monthToInt : Date.Month -> Int
-monthToInt month =
-    case month of
-        Date.Jan ->
-            1
-
-        Date.Feb ->
-            2
-
-        Date.Mar ->
-            3
-
-        Date.Apr ->
-            4
-
-        Date.May ->
-            5
-
-        Date.Jun ->
-            6
-
-        Date.Jul ->
-            7
-
-        Date.Aug ->
-            8
-
-        Date.Sep ->
-            9
-
-        Date.Oct ->
-            10
-
-        Date.Nov ->
-            11
-
-        Date.Dec ->
-            12
 
 
 dayToInt : Date.Day -> Date.Day -> Int
